bf48ba1e7ab7385a604594ffcb0c05b57f84fd20
Eduardo Hernandez <developmelk@gmail.com>
2025-04-07 08:51:02 -0600

    Update javascript.js
    Divide the javascript file by tasks

--- javascript.js
+++ javascript.js
@@ -1,5 +1,17 @@
+// Get Existing div element
 let toDoListBody = document.getElementById("toDoListBody");
-let basicButton = document.createElement("button");
 
+// Create Basic Elements and added features
+let basicButton = document.createElement("button");
 basicButton.innerText = "Agregar Tarea";
+
+let emptyTable = document.createElement();
+
+// Append to the DOM
+
 toDoListBody.append(basicButton);
+
+const addTask = function () {
+  console.log("inside");
+};
+basicButton.addEventListener("click", addTask);
