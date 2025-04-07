d43454f162c4b8813c93d3377f8e6fe88fcefa10
Eduardo Hernandez <developmelk@gmail.com>
2025-04-07 00:38:18 -0600

    Change Id
    Change div id to "toDoListBody"

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
-console.log("hi");
+let toDoListBody = document.getElementById("toDoListBody");
+let basicButton = document.createElement("button");
+
+basicButton.innerText = "Agregar Tarea";
+toDoListBody.append(basicButton);
